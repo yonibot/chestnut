4a597dd37bc27c53bbbc84ba23640fd374350ee6
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-12-18 17:18:17 +0200

    ssl fix again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: 'email, user_hometown, friends_hometown, friends_location', client_options: { ssl: { ca_file: '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
