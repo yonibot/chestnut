58470ef2cdbff8f77b80c39d7110bfaf4c297315
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-12-18 17:02:39 +0200

    attempt at fb omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: 'email, user_hometown, friends_hometown, friends_location'
+  # OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], {scope: 'email, user_hometown, friends_hometown, friends_location', client_options: { ssl: { ca_file: '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
 
 
@@ -16,3 +16,7 @@ end
 #     provider :facebook, 'DEPLOY_APP_ID', 'DEPLOY_APP_SECRET'
 #   end
 # end
+
+
+provider :facebook, 'YOUR_APP_ID', 'YOUR_SECRET_KEY', 
+           {:scope => 'PERMISSION_1, PERMISSION_2, PERMISSION_3...', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
