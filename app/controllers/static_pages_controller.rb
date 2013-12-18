e8dd14a47e2ec7a417f70b795113be86582b8226
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-12-18 16:53:28 +0200

    omniauth fb fix ssl

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,7 @@ class StaticPagesController < ApplicationController
 
 
   def index
-    if current_user
-      @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friends_with_chestnut, owner_type: "User")
-    end
+    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friends_with_chestnut, owner_type: "User") unless !current_user
   end
 
 end
