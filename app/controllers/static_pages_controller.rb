aad3327d1ed46f94e274c38446f0e040e4c12ca0
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-12-19 16:11:01 +0200

    bug fixing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
 
   def index
     if current_user
-      @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friends_with_chestnut, owner_type: "User") unless !current_user
+      @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friends_with_chestnut, owner_type: "User")
     end
   end
 
