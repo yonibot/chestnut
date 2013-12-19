0ff6862104ce74ea47e2260486d10b926f2e8b90
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-12-19 16:20:52 +0200

    big fixing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
 
   def index
-    if current_user
+    if current_user && current_user.fb_friends
       @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friends_with_chestnut, owner_type: "User")
     end
   end
