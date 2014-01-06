3939e17feddb89eb0739d7aca619f10fc9b38ca7
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2014-01-06 09:44:56 +0200

    Removed mailer for now

--- app/mailers/chestnut_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ChestnutMailer < ActionMailer::Base
-
-  def send_blog_update(user)
-    @user = user
-    mail from: 'Admin <yoni@yoniweisbrod.com>', to: user.email, subject: "New blog post on Chestnut!"
-  end
-
-
-end
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Chestnut2::Application.initialize!
-
-# Recognize Haml in Actionmailer
-ActionMailer::Base.register_template_extension('haml')
